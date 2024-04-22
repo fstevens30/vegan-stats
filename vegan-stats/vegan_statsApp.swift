c61bdc5e8a0ff5a8a1ecf6dc3e89f3070eb5c2d5
Flynn Stevens <flynnstevens30@gmail.com>
2024-04-22 14:23:12 +1200

    Initial Commit

---
+++ vegan-stats/vegan_statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  vegan_statsApp.swift
+//  vegan-stats
+//
+//  Created by Flynn Stevens on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct vegan_statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
